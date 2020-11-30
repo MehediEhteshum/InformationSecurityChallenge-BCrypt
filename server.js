13989506df4409b89cfe56b4af4758debb7e88ab
Mehedi Ehteshum <me69oshan@gmail.com>
2020-11-29 17:02:25 -0700

    Hash and Compare Passwords Synchronously;

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+//START_SYNC
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
+//END_SYNC
+
 //START_ASYNC
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
   if(err) return console.log(err);
